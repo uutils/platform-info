4523df36283dd882c205df3b558e71c7c9442a77
Roy Ivy III <rivy.dev@gmail.com>
2023-02-12 10:39:01 -0600

    docs ~ revise example(s) for API revision

--- examples/ex.rs
+++ examples/ex.rs
@@ -1,13 +1,36 @@
-// `cargo run --example ex` (executes this example)
+// examples/ex.rs
+// * use `cargo run --example ex` to execute this example
+
+// spell-checker:ignore (API) nodename osname sysname
 
 use platform_info::*;
 
 fn main() {
-    let uname = PlatformInfo::new().unwrap();
-    println!("{}", uname.sysname());
-    println!("{}", uname.nodename());
-    println!("{}", uname.release());
-    println!("{}", uname.version());
-    println!("{}", uname.machine());
-    println!("{}", uname.osname());
+    let info = PlatformInfo::new().unwrap();
+    // println!("info={:#?}", info);
+
+    println!(
+        "{}",
+        (info.sysname()).unwrap_or_else(|os_s| os_s.to_string_lossy())
+    );
+    println!(
+        "{}",
+        (info.nodename()).unwrap_or_else(|os_s| os_s.to_string_lossy())
+    );
+    println!(
+        "{}",
+        (info.release()).unwrap_or_else(|os_s| os_s.to_string_lossy())
+    );
+    println!(
+        "{}",
+        (info.version()).unwrap_or_else(|os_s| os_s.to_string_lossy())
+    );
+    println!(
+        "{}",
+        (info.machine()).unwrap_or_else(|os_s| os_s.to_string_lossy())
+    );
+    println!(
+        "{}",
+        (info.osname()).unwrap_or_else(|os_s| os_s.to_string_lossy())
+    );
 }
