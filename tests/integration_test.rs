609413cba70a6caf2ade9f2eea2740e7aed3db04
Roy Ivy III <rivy.dev@gmail.com>
2023-02-12 10:38:30 -0600

    tests ~ refactor for API revision

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -6,14 +6,17 @@ use platform_info::*;
 
 #[test]
 fn platform() -> Result<(), String> {
-    let uname = PlatformInfo::new().unwrap();
+    let uname = match PlatformInfo::new() {
+        Ok(info) => info,
+        Err(error) => panic!("{}", error),
+    };
 
-    let sysname = uname.sysname();
-    let nodename = uname.nodename();
-    let release = uname.release();
-    let version = uname.version();
-    let machine = uname.machine();
-    let osname = uname.osname();
+    let sysname = (uname.sysname()).unwrap_or_else(|os_s| os_s.to_string_lossy());
+    let nodename = (uname.nodename()).unwrap_or_else(|os_s| os_s.to_string_lossy());
+    let release = (uname.release()).unwrap_or_else(|os_s| os_s.to_string_lossy());
+    let version = (uname.version()).unwrap_or_else(|os_s| os_s.to_string_lossy());
+    let machine = (uname.machine()).unwrap_or_else(|os_s| os_s.to_string_lossy());
+    let osname = (uname.osname()).unwrap_or_else(|os_s| os_s.to_string_lossy());
 
     println!("sysname = [{}]'{}'", sysname.len(), sysname);
     println!("nodename = [{}]'{}'", nodename.len(), nodename);
@@ -22,28 +25,32 @@ fn platform() -> Result<(), String> {
     println!("machine = [{}]'{}'", machine.len(), machine);
     println!("osname = [{}]'{}'", osname.len(), osname);
 
-    assert!(!uname.sysname().is_empty());
-    assert!(!uname.nodename().is_empty());
-    assert!(!uname.release().is_empty());
-    assert!(!uname.version().is_empty());
-    assert!(!uname.machine().is_empty());
-    assert!(!uname.osname().is_empty());
+    assert!(!sysname.is_empty());
+    assert!(!nodename.is_empty());
+    assert!(!release.is_empty());
+    assert!(!version.is_empty());
+    assert!(!machine.is_empty());
+    assert!(!osname.is_empty());
 
+    // assert!(false);
     Ok(())
 }
 
 #[test]
 fn platform_no_invisible_contents() -> Result<(), String> {
-    let uname = PlatformInfo::new().unwrap();
+    let uname = match PlatformInfo::new() {
+        Ok(info) => info,
+        Err(error) => panic!("{}", error),
+    };
 
-    let sysname = uname.sysname();
-    let nodename = uname.nodename();
-    let release = uname.release();
-    let version = uname.version();
-    let machine = uname.machine();
-    let osname = uname.osname();
+    let sysname = (uname.sysname()).unwrap_or_else(|os_str| os_str.to_string_lossy());
+    let nodename = (uname.nodename()).unwrap_or_else(|os_str| os_str.to_string_lossy());
+    let release = (uname.release()).unwrap_or_else(|os_str| os_str.to_string_lossy());
+    let version = (uname.version()).unwrap_or_else(|os_str| os_str.to_string_lossy());
+    let machine = (uname.machine()).unwrap_or_else(|os_str| os_str.to_string_lossy());
+    let osname = (uname.osname()).unwrap_or_else(|os_str| os_str.to_string_lossy());
 
-    let s = format!("sysname='{sysname}';nodename='{nodename}';release='{release}';version='{version}';machine='{machine}';osname={osname}");
+    let s = format!("sysname='{sysname}';nodename='{nodename}';release='{release}';version='{version}';machine='{machine}';osname='{osname}'");
     println!("s = [{}]\"{}\"", s.len(), s);
 
     // let re = regex::Regex::new("[^[[:print:]]]").unwrap(); // matches invisible (and emojis)
